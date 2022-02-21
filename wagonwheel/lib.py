503bee3fdc756ec42e8d9a294f34b102d846c421
Ruben Rossbach <ruben.rossbach@gmx.net>
2022-02-21 12:56:33 +0100

    first commit

---
+++ wagonwheel/lib.py
@@ -0,0 +1,9 @@
+def try_me():
+    print("""
+        Le Wagon goes brrrr
+           _____________
+        _/_|[][][][][]  | - -
+        (      Le Wagon | - -
+        =--OO-------OO--=
+    """
+    )
